f7d64fe3971e55f9b22415f923f087fae58da47e
kannu-jaat <krix7096@gmail.com>
2026-08-03 15:10:35 +0530

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,1 +1,18 @@
-//auto generate
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBu0OkmpY7uXZkQIlVxfQj9MCyGBOA9sxI",
+  authDomain: "whatsapp-web-03.firebaseapp.com",
+  databaseURL: "https://whatsapp-web-03-default-rtdb.firebaseio.com",
+  projectId: "whatsapp-web-03",
+  storageBucket: "whatsapp-web-03.firebasestorage.app",
+  messagingSenderId: "239319034758",
+  appId: "1:239319034758:web:1c260d221b5f698a63da07"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
